762e42a8ea1f8ac2b22754adec9c92f71943bd5b
marcin.maliszkiewicz@blockfactory.com <marcin.maliszkiewicz@proxeus.com>
2018-10-09 16:18:48 +0200

    update prepare-release.sh

--- prepare-release.sh
+++ prepare-release.sh
@@ -5,13 +5,13 @@ VERSION=$1
 
 SRC_PATH=$(go env GOPATH)/src/git.proxeus.com/core/central/artifacts/dapp/
 
-NAME_DARWIN=darwin-amd64/Proxeus.app.zip
+NAME_DARWIN=darwin-amd64/Proxeus.app/Contents/MacOS/Proxeus
 NAME_LINUX=linux-amd64/Proxeus
 NAME_WINDOWS=windows-amd64/Proxeus.exe
 
 REL_PREFIX=./releases/${VERSION}/
 
-mkdir -p ${REL_PREFIX}darwin-amd64/
+mkdir -p ${REL_PREFIX}darwin-amd64/Proxeus.app/Contents/MacOS
 mkdir -p ${REL_PREFIX}linux-amd64/
 mkdir -p ${REL_PREFIX}windows-amd64/
 
@@ -19,18 +19,17 @@ cp ${SRC_PATH}${NAME_DARWIN} ${REL_PREFIX}${NAME_DARWIN}
 cp ${SRC_PATH}${NAME_LINUX} ${REL_PREFIX}${NAME_LINUX}
 cp ${SRC_PATH}${NAME_WINDOWS} ${REL_PREFIX}${NAME_WINDOWS}
 
-URL_PREFIX=_/releases/
+URL_PREFIX=https://github.com/ProxeusApp/dappRelease/raw/master/releases/
 
 echo "{
-	\"version\": \"$VERSION\"
-	\"darwin\": \"${URL_PREFIX}${NAME_DARWIN}\"
-	\"linux\": \"${URL_PREFIX}${NAME_LINUX}\"
-	\"windows\": \"${URL_PREFIX}${NAME_WINDOWS}\"
+	\"Version\": \"${VERSION}\",
+	\"Darwin\":  \"${URL_PREFIX}${VERSION}/${NAME_DARWIN}\",
+	\"Linux\":   \"${URL_PREFIX}${VERSION}/${NAME_LINUX}\",
+	\"Windows\": \"${URL_PREFIX}${VERSION}/${NAME_WINDOWS}\"
 }" > ./versions_v1
 
 git add ./releases
 git add versions_v1
 git commit -m "publish release "$VERSION
 
-echo "type this to release:"
-echo "git push origin HEAD"
+echo "type this to release: git push origin HEAD"
